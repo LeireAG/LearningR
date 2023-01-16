e6260a11ab1122ab178594ed717fdb192ab51918
Leire <leire.alonso@scilifelab.se>
2023-01-16 16:06:52 +0100

    Added R files to history.

---
+++ R/learning.R
@@ -0,0 +1,24 @@
+10
+
+# R basics  ---------------------------------------------------------------
+
+
+weight_kilos <- 100
+weight_kilos
+weight_kilos <- 10
+weight_kilos
+colnames(airquality)
+colnames()
+str(airquality)
+
+summary(airquality)
+
+2 + 2
+
+2 + 2
+
+# Packages  ---------------------------------------------------------------
+
+
+library(tidyverse)
+r3::check_git_config()
